3a4f03167914878ed66d57b4a33266c7a16534f8
jhl221123 <jhl221123@naver.com>
2023-04-02 12:52:56 +0900

    Add for read of item board

---
+++ src/main/java/com/catdog/help/web/form/itemBoard/ReadItemBoardForm.java
@@ -0,0 +1,24 @@
+package com.catdog.help.web.form.itemBoard;
+
+import com.catdog.help.domain.Dates;
+import com.catdog.help.domain.board.ItemStatus;
+import com.catdog.help.domain.board.UploadFile;
+import com.catdog.help.domain.user.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+public class ReadItemBoardForm {
+    private Long id;
+    private User user;
+    private String title;
+    private String content;
+    private Dates dates;
+    private String itemName;
+    private int price;
+    private ItemStatus status;
+    private List<UploadFile> images = new ArrayList<>();
+}
