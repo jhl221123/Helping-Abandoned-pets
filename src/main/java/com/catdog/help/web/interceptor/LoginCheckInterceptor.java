8ec0d38280612d256e93eefa479c5bbb422cf376
jhl221123 <jhl221123@naver.com>
2023-04-04 15:52:27 +0900

    Add parameter to redirect path when checking interceptor for list of boards

--- src/main/java/com/catdog/help/web/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/catdog/help/web/interceptor/LoginCheckInterceptor.java
@@ -14,10 +14,15 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
+        String param = request.getParameter("page");
 
         HttpSession session = request.getSession();
         if (session == null || session.getAttribute(SessionConst.LOGIN_USER) == null) {
-            response.sendRedirect("/users/login?redirectURL=" + requestURI);
+            if (param != null) {
+                response.sendRedirect("/users/login?redirectURL=" + requestURI + "?page=" + param);
+            } else {
+                response.sendRedirect("/users/login?redirectURL=" + requestURI);
+            } 
             return false;
         }
 
