2a79c6593bf51ae74f94c898d9a9973b100b7b19
jhl221123 <jhl221123@naver.com>
2023-04-05 18:04:24 +0900

    Add entity for message system

---
+++ src/main/java/com/catdog/help/domain/message/Message.java
@@ -0,0 +1,34 @@
+package com.catdog.help.domain.message;
+
+import com.catdog.help.domain.Dates;
+import com.catdog.help.domain.user.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Message {
+
+    @Id @GeneratedValue
+    @Column(name = "message_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "message_room_id")
+    private MessageRoom messageRoom;
+
+    private String content;
+
+    private Dates dates;
+
+    public void addMessage(MessageRoom messageRoom) {
+        this.messageRoom = messageRoom;
+        messageRoom.getMessages().add(this);
+    }
+}
---
+++ src/main/java/com/catdog/help/domain/message/MessageRoom.java
@@ -0,0 +1,34 @@
+package com.catdog.help.domain.message;
+
+import com.catdog.help.domain.board.ItemBoard;
+import com.catdog.help.domain.user.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class MessageRoom {
+
+    @Id @GeneratedValue
+    @Column(name = "message_room_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private ItemBoard itemBoard;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipient_id")
+    private User recipient;
+
+    @OneToMany(mappedBy = "messageRoom", cascade = CascadeType.REMOVE)
+    private List<Message> messages = new ArrayList<>();
+}
