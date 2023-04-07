bb60da68fcf262b78f2450680dafe8f36b10598e
jhl221123 <jhl221123@naver.com>
2023-04-07 16:07:21 +0900

    Add find method using join fetch

--- src/main/java/com/catdog/help/repository/jpa/MessageRoomRepository.java
+++ src/main/java/com/catdog/help/repository/jpa/MessageRoomRepository.java
@@ -18,9 +18,28 @@ public class MessageRoomRepository {
         return messageRoom.getId();
     }
 
+    public MessageRoom findById(Long id) {
+        return em.find(MessageRoom.class, id);
+    }
+
+    public MessageRoom findWithRefers(Long id) {
+        return em.createQuery("select mr from MessageRoom mr" +
+                        " join fetch mr.itemBoard i" +
+                        " join fetch mr.sender s" +
+                        " join fetch mr.recipient r" +
+                        " join fetch mr.messages ms" +
+                        " where mr.id = :id", MessageRoom.class)
+                .setParameter("id", id)
+                .getResultList()
+                .stream()
+                .findAny()
+                .orElse(null);
+    }
+
     public List<MessageRoom> findAllByUserId(Long userId) {
         return em.createQuery("select mr from MessageRoom mr where sender_id = :userId or recipient_id = :userId", MessageRoom.class)
                 .setParameter("userId", userId)
                 .getResultList();
+        // TODO: 2023-04-06 생성일 추가 후  order by create_date desc
     }
 }
