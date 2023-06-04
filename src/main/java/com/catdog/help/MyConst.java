72fa349fbf879da9105e89139c5ee364f0896bd4
jhl221123 <jhl221123@naver.com>
2023-06-04 17:48:49 +0900

    docs: Add api to read user info

--- src/main/java/com/catdog/help/MyConst.java
+++ src/main/java/com/catdog/help/MyConst.java
@@ -1,6 +1,9 @@
 package com.catdog.help;
 
 public abstract class MyConst {
+    //common
+    public static final String ID = "id";
+    public static final String CREATED_DATE = "createdDate";
 
     //user
     public static final String EMAIL = "emailId";
@@ -12,8 +15,14 @@ public abstract class MyConst {
     public static final String FAIL_LOGIN = "fail_login";
 
     //board
-    public static final String BULLETIN = "bulletin";
     public static final String LOST = "lost";
+    public static final String BULLETIN = "bulletin";
     public static final String ITEM = "item";
     public static final String INQUIRY = "inquiry";
+    public static final String LOST_SIZE = "lostSize";
+    public static final String BULLETIN_SIZE = "bulletinSize";
+    public static final String ITEM_SIZE = "itemSize";
+    public static final String INQUIRY_SIZE = "inquirySize";
+    public static final String LIKE_BULLETIN_SIZE = "likeBulletinSize";
+    public static final String LIKE_ITEM_SIZE = "likeItemSize";
 }
--- src/test/java/com/catdog/help/web/api/docs/UserApiControllerDocsTest.java
+++ src/test/java/com/catdog/help/web/api/docs/UserApiControllerDocsTest.java
@@ -5,7 +5,11 @@ import com.catdog.help.domain.user.User;
 import com.catdog.help.repository.UserRepository;
 import com.catdog.help.web.api.request.user.LoginRequest;
 import com.catdog.help.web.api.request.user.SaveUserRequest;
+import com.catdog.help.web.api.response.user.ReadUserResponse;
+import com.catdog.help.web.form.user.ReadUserForm;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,13 +28,14 @@ import java.util.List;
 import static com.catdog.help.MyConst.*;
 import static com.catdog.help.web.SessionConst.LOGIN_USER;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @Transactional
@@ -64,17 +69,17 @@ public class UserApiControllerDocsTest {
                 .andExpect(status().isOk())
                 .andDo(document("users-new",
                         requestFields(
-                                fieldWithPath("emailId").description("이메일")
+                                fieldWithPath(EMAIL).description("이메일")
                                         .attributes(key("constraints").value(getConstraintDescription(SaveUserRequest.class, EMAIL))),
-                                fieldWithPath("password").description("비밀번호")
+                                fieldWithPath(PASSWORD).description("비밀번호")
                                         .attributes(key("constraints").value(getConstraintDescription(SaveUserRequest.class, PASSWORD))),
-                                fieldWithPath("name").description("이름")
+                                fieldWithPath(NAME).description("이름")
                                         .attributes(key("constraints").value(getConstraintDescription(SaveUserRequest.class, NAME))),
-                                fieldWithPath("nickname").description("닉네임")
+                                fieldWithPath(NICKNAME).description("닉네임")
                                         .attributes(key("constraints").value(getConstraintDescription(SaveUserRequest.class, NICKNAME))),
-                                fieldWithPath("age").description("나이")
+                                fieldWithPath(AGE).description("나이")
                                         .attributes(key("constraints").value(getConstraintDescription(SaveUserRequest.class, AGE))),
-                                fieldWithPath("gender").description("성별")
+                                fieldWithPath(GENDER).description("성별")
                                         .attributes(key("constraints").value(getConstraintDescription(SaveUserRequest.class, GENDER)))
                         ),
                         responseFields(
@@ -105,9 +110,9 @@ public class UserApiControllerDocsTest {
                 .andExpect(request().sessionAttribute(LOGIN_USER, "닉네임"))
                 .andDo(document("users-login",
                         requestFields(
-                                fieldWithPath("emailId").description("이메일")
+                                fieldWithPath(EMAIL).description("이메일")
                                         .attributes(key("constraints").value(getConstraintDescription(SaveUserRequest.class, EMAIL))),
-                                fieldWithPath("password").description("비밀번호")
+                                fieldWithPath(PASSWORD).description("비밀번호")
                                         .attributes(key("constraints").value(getConstraintDescription(SaveUserRequest.class, PASSWORD)))
                         ),
                         responseFields(
@@ -133,21 +138,61 @@ public class UserApiControllerDocsTest {
                 .andDo(document("users-logout"));
     }
 
+    @Test
+    @DisplayName("Docs 내 정보 조회")
+    void readLoginUser() throws Exception {
+        //given
+        User user = getUser();
+        userRepository.save(user);
 
-    private User getUser() {
-        return User.builder()
-                .emailId("test@test.test")
-                .password("12341234")
-                .nickname("닉네임")
-                .name("이름")
-                .age(22)
-                .gender(Gender.WOMAN)
-                .build();
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(LOGIN_USER, "닉네임");
+
+        ReadUserResponse response = getReadUserResponse(user);
+        String result = objectMapper
+                .registerModule(new JavaTimeModule()) //LocalDateTime 직렬화
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .writeValueAsString(response);
+
+        //expected
+        mockMvc.perform(get("/api/users/detail")
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON)
+                        .header("Set-Cookie", session.getId())
+                        .session(session)
+                )
+                .andExpect(content().json(result))
+                .andDo(document("users-Info",
+                        requestHeaders(headerWithName("Set-Cookie").description("사용자 인증 쿠키")),
+                        responseFields(
+                                fieldWithPath(ID).description("사용자 식별자"),
+                                fieldWithPath(EMAIL).description("이메일"),
+                                fieldWithPath(NAME).description("이름"),
+                                fieldWithPath(NICKNAME).description("닉네임"),
+                                fieldWithPath(AGE).description("나이"),
+                                fieldWithPath(GENDER).description("성별"),
+                                fieldWithPath(CREATED_DATE).description("가입날짜"),
+                                fieldWithPath(LOST_SIZE).description("작성한 실종글 수"),
+                                fieldWithPath(BULLETIN_SIZE).description("작성한 게시글 수"),
+                                fieldWithPath(ITEM_SIZE).description("작성한 나눔글 수"),
+                                fieldWithPath(INQUIRY_SIZE).description("작성한 문의글 수"),
+                                fieldWithPath(LIKE_BULLETIN_SIZE).description("좋아요 누른 게시글 수"),
+                                fieldWithPath(LIKE_ITEM_SIZE).description("좋아요 누른 나눔글 수")
+                        )
+                ));
     }
 
-    private List<String> getConstraintDescription(Class target, String field) {
-        ConstraintDescriptions simpleRequestConstraints = new ConstraintDescriptions(target);
-        return simpleRequestConstraints.descriptionsForProperty(field);
+
+    private ReadUserResponse getReadUserResponse(User user) {
+        return ReadUserResponse.builder()
+                .form(new ReadUserForm(user))
+                .lostSize(0L)
+                .bulletinSize(0L)
+                .itemSize(0L)
+                .inquirySize(0L)
+                .likeBulletinSize(0L)
+                .likeItemSize(0L)
+                .build();
     }
 
     private SaveUserRequest getSaveUserRequest() {
@@ -158,6 +203,22 @@ public class UserApiControllerDocsTest {
                 .nickname("닉네임")
                 .age(22)
                 .gender(Gender.MAN)
+                .build();
+    }
+
+    private List<String> getConstraintDescription(Class target, String field) {
+        ConstraintDescriptions simpleRequestConstraints = new ConstraintDescriptions(target);
+        return simpleRequestConstraints.descriptionsForProperty(field);
+    }
+
+    private User getUser() {
+        return User.builder()
+                .emailId("test@test.test")
+                .password("12341234")
+                .nickname("닉네임")
+                .name("이름")
+                .age(22)
+                .gender(Gender.WOMAN)
                 .build();
     }
 }
