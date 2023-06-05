440af99451d3bcc921ae6533affb9c24a400cac7
jhl221123 <jhl221123@naver.com>
2023-06-05 13:15:36 +0900

    feat: Add bulletin board page read api

--- src/main/java/com/catdog/help/web/api/controller/BulletinApiController.java
+++ src/main/java/com/catdog/help/web/api/controller/BulletinApiController.java
@@ -5,16 +5,21 @@ import com.catdog.help.service.BulletinService;
 import com.catdog.help.service.CommentService;
 import com.catdog.help.service.LikeService;
 import com.catdog.help.web.api.request.bulletin.SaveBulletinRequest;
+import com.catdog.help.web.api.response.bulletin.PageBulletinResponse;
 import com.catdog.help.web.api.response.bulletin.SaveBulletinResponse;
 import com.catdog.help.web.controller.ViewUpdater;
+import com.catdog.help.web.form.bulletin.PageBulletinForm;
 import com.catdog.help.web.form.bulletin.SaveBulletinForm;
+import com.catdog.help.web.form.search.BulletinSearch;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.data.domain.Sort.Direction.DESC;
 
 @Slf4j
 @RestController
@@ -34,5 +39,19 @@ public class BulletinApiController {
     public SaveBulletinResponse save(@RequestBody @Validated SaveBulletinRequest request) {
         Long boardId = bulletinService.save(new SaveBulletinForm(request), request.getNickname());
         return new SaveBulletinResponse(boardId);
+    }
+
+    /***  read  ***/
+    @GetMapping
+    public PageBulletinResponse getPage(@RequestBody BulletinSearch search,
+                                        @PageableDefault(sort = "id", direction = DESC) Pageable pageable) {
+        Page<PageBulletinForm> pageForms = bulletinService.search(search, pageable);
+        return PageBulletinResponse.builder()
+                .content(pageForms.getContent())
+                .page(pageForms.getPageable().getPageNumber())
+                .size(pageForms.getPageable().getPageSize())
+                .totalElements(pageForms.getTotalElements())
+                .totalPages(pageForms.getTotalPages())
+                .build();
     }
 }
--- src/test/java/com/catdog/help/web/api/controller/BulletinApiControllerTest.java
+++ src/test/java/com/catdog/help/web/api/controller/BulletinApiControllerTest.java
@@ -2,8 +2,11 @@ package com.catdog.help.web.api.controller;
 
 import com.catdog.help.service.BulletinService;
 import com.catdog.help.web.api.request.bulletin.SaveBulletinRequest;
+import com.catdog.help.web.api.response.bulletin.PageBulletinResponse;
 import com.catdog.help.web.api.response.bulletin.SaveBulletinResponse;
+import com.catdog.help.web.form.bulletin.PageBulletinForm;
 import com.catdog.help.web.form.bulletin.SaveBulletinForm;
+import com.catdog.help.web.form.search.BulletinSearch;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,20 +15,27 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @ExtendWith(MockitoExtension.class)
-@Transactional
 class BulletinApiControllerTest {
 
     @InjectMocks
@@ -66,6 +76,44 @@ class BulletinApiControllerTest {
                         .content(json)
                 )
                 .andExpect(MockMvcResultMatchers.content().json(result));
+    }
+
+    @Test
+    @DisplayName("검색 조건에 맞는 게시글 페이지 조회")
+    void getPage() throws Exception {
+        //given
+        BulletinSearch search = BulletinSearch.builder()
+                .region("부산")
+                .title("검색제목")
+                .build();
+        String request = objectMapper.writeValueAsString(search);
+
+        List<PageBulletinForm> forms = new ArrayList<>();
+        Page<PageBulletinForm> pageBulletinForms = new PageImpl<>(forms, PageRequest.of(0, 10), 0);
+
+        PageBulletinResponse response = getPageBulletinResponse(pageBulletinForms);
+        String result = objectMapper.writeValueAsString(response);
+
+        doReturn(pageBulletinForms).when(bulletinService)
+                .search(any(BulletinSearch.class), any(Pageable.class));
+
+        //expected
+        mockMvc.perform(get("/api/bulletins")
+                        .contentType(APPLICATION_JSON)
+                        .content(request)
+                )
+                .andExpect(content().json(result));
+    }
+
+
+    private PageBulletinResponse getPageBulletinResponse(Page<PageBulletinForm> pageBulletinForms) {
+        return PageBulletinResponse.builder()
+                .content(pageBulletinForms.getContent())
+                .page(pageBulletinForms.getPageable().getPageNumber())
+                .size(pageBulletinForms.getPageable().getPageSize())
+                .totalElements(pageBulletinForms.getTotalElements())
+                .totalPages(pageBulletinForms.getTotalPages())
+                .build();
     }
 
     private SaveBulletinResponse getSaveBulletinResponse() {
