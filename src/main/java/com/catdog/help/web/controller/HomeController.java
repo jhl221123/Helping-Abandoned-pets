1c4d4f6e250875f5f919f4635cc14a348da2da01
jhl221123 <jhl221123@naver.com>
2023-06-07 12:44:12 +0900

    docs: Add table

--- src/main/java/com/catdog/help/web/controller/HomeController.java
+++ src/main/java/com/catdog/help/web/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.catdog.help.web.controller;
 
 import com.catdog.help.service.BulletinService;
+import com.catdog.help.service.ItemService;
+import com.catdog.help.service.LostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,12 +14,20 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class HomeController {
 
+    private final LostService lostService;
     private final BulletinService bulletinService;
+    private final ItemService itemService;
 
     @GetMapping("/")
     public String home(Model model) {
-        Map<String, Long> countMap = bulletinService.getCountByRegion();
-        model.addAttribute("countMap", countMap);
+        Map<String, Long> lostMap = lostService.getCountByRegion();
+        model.addAttribute("lostMap", lostMap);
+
+        Map<String, Long> bulletinMap = bulletinService.getCountByRegion();
+        model.addAttribute("bulletinMap", bulletinMap);
+
+        Map<String, Long> itemMap = itemService.getCountByRegion();
+        model.addAttribute("itemMap", itemMap);
 
         return "home";
     }
