4b2fea363398ccbd0dc9d7f527a19dca29846fcd
jhl221123 <jhl221123@naver.com>
2023-07-02 15:49:26 +0900

    feat: Add inquiry board page api

--- src/main/java/com/catdog/help/web/api/controller/InquiryApiController.java
+++ src/main/java/com/catdog/help/web/api/controller/InquiryApiController.java
@@ -2,14 +2,20 @@ package com.catdog.help.web.api.controller;
 
 import com.catdog.help.service.InquiryService;
 import com.catdog.help.web.api.request.inquiry.SaveInquiryRequest;
+import com.catdog.help.web.api.response.inquiry.PageInquiryResponse;
 import com.catdog.help.web.api.response.inquiry.ReadInquiryResponse;
 import com.catdog.help.web.api.response.inquiry.SaveInquiryResponse;
 import com.catdog.help.web.form.inquiry.ReadInquiryForm;
 import com.catdog.help.web.form.inquiry.SaveInquiryForm;
+import com.catdog.help.web.form.search.InquirySearch;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import static com.catdog.help.web.SessionConst.LOGIN_USER;
+import static org.springframework.data.domain.Sort.Direction.DESC;
 
 @RestController
 @RequestMapping("/api/inquiries")
@@ -23,6 +29,12 @@ public class InquiryApiController {
     public SaveInquiryResponse saveBoard(@SessionAttribute(LOGIN_USER) String nickname, @RequestBody SaveInquiryRequest request) {
         Long savedId = inquiryService.save(new SaveInquiryForm(request), nickname);
         return new SaveInquiryResponse(savedId);
+    }
+
+    @GetMapping
+    public Page<PageInquiryResponse> readPage(@RequestBody InquirySearch search,
+                                              @PageableDefault(size = 5, direction = DESC) Pageable pageable) {
+        return inquiryService.search(search, pageable).map(PageInquiryResponse::new);
     }
 
     @GetMapping("/{id}")
